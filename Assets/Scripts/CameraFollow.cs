bba95f0904c77ad60bf3a1b67a27fac8b82d2ee7
Jefferson <jeffersonferreira.01@pm.me>
2022-08-27 14:41:58 -0300

    ✨ Added camera follow behavior

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public Transform PlayerTransform;
+	[SerializeField] private float _smoothTime;
+
+	private Vector3 _offset;
+	private Vector3 _currVel;
+
+	private void Start()
+	{
+		ComputeCurrentOffset();
+	}
+
+	private void LateUpdate()
+	{
+		Vector3 current = transform.position;
+		Vector3 target = PlayerTransform.position + _offset;
+
+		transform.position = Vector3.SmoothDamp(current, target, ref _currVel, _smoothTime);
+	}
+
+	private void ComputeCurrentOffset()
+	{
+		_offset = transform.position - PlayerTransform.position;
+	}
+}
