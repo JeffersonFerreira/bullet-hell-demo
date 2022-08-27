2912f2f939f1c05d4c776875d2568ad76ad6ac11
Jefferson <jeffersonferreira.01@pm.me>
2022-08-27 20:08:20 -0300

    ✨ Added heath to player

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+	public class PlayerHealth : MonoBehaviour
+	{
+		[SerializeField] private int _initialHealth = 3;
+		[SerializeField] private float _invulnerabilityDuration = 0.2f;
+
+		public event Action<int> OnTakeDamage;
+
+		public int Health { get; private set; }
+
+		private float _lastDamageTime = float.NegativeInfinity;
+
+		private void Awake()
+		{
+			Health = _initialHealth;
+		}
+
+		public void TakeDamage()
+		{
+			if (CanTakeDamage())
+			{
+				Health--;
+				_lastDamageTime = Time.time;
+				OnTakeDamage?.Invoke(Health);
+			}
+		}
+
+		public bool CanTakeDamage()
+		{
+			return Time.time > _lastDamageTime + _invulnerabilityDuration;
+		}
+	}
+}
