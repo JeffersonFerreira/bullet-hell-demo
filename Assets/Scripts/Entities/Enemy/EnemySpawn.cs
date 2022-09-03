6af9d2f1d2c883584aa6e048ee3b7ac566291492
Jefferson <jeffersonferreira.01@pm.me>
2022-09-03 19:56:26 -0300

    ✨ Added enemy spawner

---
+++ Assets/Scripts/Entities/Enemy/EnemySpawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Entity.Enemy
+{
+	public class EnemySpawn : MonoBehaviour
+	{
+		[SerializeField] private float _spawnEverySeconds = 10;
+
+		[Space]
+		[SerializeField] private Transform[] _spawnPoints;
+		[SerializeField] private BasicEnemy[] _enemies;
+
+		private IEnumerator Start()
+		{
+			int _spawnIndex = -1;
+			while (true)
+			{
+				_spawnIndex = (_spawnIndex + 1) % _spawnPoints.Length;
+
+				int i = Random.Range(0, _enemies.Length);
+
+				Transform spawnPoint = _spawnPoints[_spawnIndex];
+				BasicEnemy prefab = _enemies[i];
+
+				Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+				yield return new WaitForSeconds(_spawnEverySeconds);
+			}
+		}
+	}
+}
