f17d1e5667f264895023e1d3d554941d88b75a7c
Jefferson <jeffersonferreira.01@pm.me>
2022-09-03 19:40:44 -0300

    🎨 Small improvement to boss controller script

--- Assets/Scripts/Entities/Enemy/Boss/BossController.cs
+++ Assets/Scripts/Entities/Enemy/Boss/BossController.cs
@@ -12,53 +12,43 @@ namespace Entity.Enemy.Boss
 
 		private Transform _playerTransform;
 
-		private IEnumerator Start()
+		private void Awake()
 		{
 			_playerTransform = GameObject.FindWithTag("Player").transform;
-			yield return SpawnArcRoutine(new[] {9, 10}, 5);
-			yield return SpawnArcRoutine(new[] {10}, 5);
-			yield return VolleyRoutine(50);
-
-			yield return VolleyArc(20, 5);
-			yield return VolleyArc(20, 10, 0.01f);
 		}
 
-		private IEnumerator VolleyRoutine(int amount)
+		private IEnumerator Start()
 		{
-			for (var i = 0; i < amount; i++)
-			{
-				Vector3 pos = transform.position;
-				Vector3 playerDir = (_playerTransform.position - pos).normalized;
-				Vector3 origin = pos + playerDir * 3;
+			int volleyCount = 0;
 
-				Fire(origin, playerDir, 10);
-				yield return new WaitForSeconds(0.15f);
+			while (true)
+			{
+				if (volleyCount >= 2 || Random.Range(0, 3) == 0)
+				{
+					volleyCount = Mathf.Max(0, volleyCount - 1);
+					yield return UseRandomPattern();
+				}
+				else
+				{
+					volleyCount++;
+					int amount = Random.Range(25, 40);
+					float bulletSpeed = Random.Range(8f, 18f);
+					yield return VolleyRoutine(amount, bulletSpeed);
+				}
+
+				yield return new WaitForSeconds(1f);
 			}
 		}
 
-		private IEnumerator VolleyArc(int bulletsPerRow, int rows = 1, float delay = 0.05f)
+		private IEnumerator UseRandomPattern()
 		{
-			Vector3 pos = transform.position;
-			Vector3 forward = transform.forward;
+			int patternToUse = Random.Range(0, 4);
 
-			var arcDirs = new List<Vector3>();
-
-			for (var i = 0; i < rows; i++)
-			{
-				bool reverse = i % 2 == 0;
-
-				arcDirs.AddRange(!reverse
-					? ComputeArcPoints(bulletsPerRow, forward)
-					: ComputeArcPoints(bulletsPerRow, forward).Reverse()
-				);
-			}
-
-			var wait = new WaitForSeconds(delay);
-			foreach (Vector3 dir in arcDirs)
-			{
-				Vector3 origin = pos + dir * 3;
-				Fire(origin, dir, 5);
-				yield return wait;
+			switch (patternToUse) {
+				case 0: yield return FullArcRoutine(new[] {9, 10}, 5); break;
+				case 1: yield return FullArcRoutine(new[] {10}, 5); break;
+				case 2: yield return VolleyArcRoutine(20, 5); break;
+				case 3: yield return VolleyArcRoutine(20, 10, 0.01f); break;
 			}
 		}
 
@@ -74,30 +64,59 @@ namespace Entity.Enemy.Boss
 			});
 		}
 
-		private IEnumerator SpawnArcRoutine(int[] pattern, int repeatCount)
+		private IEnumerator VolleyRoutine(int amount, float bulletSpeed)
+		{
+			for (var i = 0; i < amount; i++)
+			{
+				Vector3 pos = transform.position;
+				Vector3 playerDir = (_playerTransform.position - pos).normalized;
+				Vector3 origin = pos + playerDir * 3;
+
+				Fire(origin, playerDir, bulletSpeed);
+				yield return new WaitForSeconds(0.15f);
+			}
+		}
+
+		private IEnumerator VolleyArcRoutine(int bulletsPerRow, int rows = 1, float fireDelay = 0.05f)
+		{
+			Vector3 pos = transform.position;
+			Vector3 forward = transform.forward;
+
+			var volleyArcDirections = Enumerable
+				.Range(0, rows)
+				.SelectMany(index => {
+					bool reverse = index % 2 == 0;
+					return !reverse
+						? ComputeArcDirections(bulletsPerRow, forward)
+						: ComputeArcDirections(bulletsPerRow, forward).Reverse();
+				});
+
+			var wait = new WaitForSeconds(fireDelay);
+			foreach (Vector3 dir in volleyArcDirections)
+			{
+				Vector3 origin = pos + dir * 3;
+				Fire(origin, dir, 5);
+				yield return wait;
+			}
+		}
+
+		private IEnumerator FullArcRoutine(int[] pattern, int repeatCount)
 		{
 			Vector3 pos = transform.position;
 
 			foreach (int spawnCount in Enumerable.Repeat(pattern, repeatCount).SelectMany(arr => arr))
 			{
-				foreach (Vector3 dir in ComputeArcPoints(spawnCount, transform.forward))
+				foreach (Vector3 dir in ComputeArcDirections(spawnCount, transform.forward))
 				{
 					Vector3 origin = pos + dir * 3;
-					Bullet bullet = Instantiate(_bossBullet, origin, Quaternion.LookRotation(dir));
-
-					bullet.Apply(new BulletData {
-						Speed = 3,
-						Origin = origin,
-						SourceInstanceID = gameObject.GetInstanceID(),
-						Timeout = 10
-					});
+					Fire(origin, dir, 3);
 				}
 
 				yield return new WaitForSeconds(0.5f);
 			}
 		}
 
-		private static IEnumerable<Vector3> ComputeArcPoints(int amount, Vector3 dir)
+		private static IEnumerable<Vector3> ComputeArcDirections(int amount, Vector3 dir)
 		{
 			amount++;
 
