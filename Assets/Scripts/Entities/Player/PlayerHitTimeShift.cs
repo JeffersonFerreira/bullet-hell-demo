cf194150c9abae31f989a461e08fc5e5a7dd5537
Jefferson <jeffersonferreira.01@pm.me>
2022-09-05 18:37:09 -0300

    ✨ Added player hurt time shift

---
+++ Assets/Scripts/Entities/Player/PlayerHitTimeShift.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Entity.Player
+{
+	public class PlayerHitTimeShift : MonoBehaviour
+	{
+		[SerializeField] private float _duration = 0.2f;
+		[SerializeField] private float _smoothTime = 0.1f;
+
+		[Range(0, 1)]
+		[SerializeField] private float _slowScale;
+
+		private BaseHealthSystem _healthSystem;
+		private Coroutine _coroutine;
+
+		private void Awake()
+		{
+			_healthSystem = GetComponent<BaseHealthSystem>();
+		}
+
+		private void Start()
+		{
+			_healthSystem.OnTakeDamage += HealthSystem_OnTakeDamage;
+		}
+
+		private void HealthSystem_OnTakeDamage(DamageData _)
+		{
+			if (_coroutine != null)
+				StopCoroutine(_coroutine);
+
+			_coroutine = StartCoroutine(TimeShiftRoutine());
+		}
+
+		private IEnumerator TimeShiftRoutine()
+		{
+			float vel = 0;
+
+			// Perform slowness
+			while (Time.timeScale > _slowScale)
+			{
+				Time.timeScale = Mathf.SmoothDamp(Time.timeScale, _slowScale, ref vel, _smoothTime, float.PositiveInfinity, Time.fixedUnscaledDeltaTime);
+				yield return null;
+			}
+
+			float t = _duration;
+			while (t > 0)
+			{
+				t -= Time.fixedUnscaledDeltaTime;
+				yield return null;
+			}
+
+			vel = 0;
+			// Perform time normalization
+			while (Time.timeScale < 1)
+			{
+				Time.timeScale = Mathf.SmoothDamp(Time.timeScale, 1, ref vel, _smoothTime, float.PositiveInfinity, Time.fixedUnscaledDeltaTime);
+				yield return null;
+			}
+
+			_coroutine = null;
+		}
+	}
+}
--- Assets/Scripts/Guns/Bullet.cs
+++ Assets/Scripts/Guns/Bullet.cs
@@ -7,10 +7,12 @@ namespace Guns
 	public class Bullet : MonoBehaviour
 	{
 		[SerializeField] private BulletHitEffect _hitEffect;
+		[SerializeField] private float _onHitDestroyDelay = 0.2f;
 
 		public BulletData Data { get; private set; }
 
 		private Rigidbody _rigidbody;
+		private TrailRenderer _trailRenderer;
 
 		private const int LAYER_HIT_ENEMY = 11;
 		private const int LAYER_HIT_PLAYER = 10;
@@ -18,6 +20,7 @@ namespace Guns
 		private void Awake()
 		{
 			_rigidbody = GetComponent<Rigidbody>();
+			_trailRenderer = GetComponentInChildren<TrailRenderer>();
 		}
 
 		public void Apply(BulletData data)
@@ -33,27 +36,38 @@ namespace Guns
 
 		private void OnCollisionEnter(Collision collision)
 		{
+			enabled = false;
+
+			if (_onHitDestroyDelay <= 0)
+				Dispose();
+			else
+				Invoke(nameof(Dispose), _onHitDestroyDelay);
+
 			switch (_hitEffect)
 			{
 				case BulletHitEffect.Damage:
 				{
 					if (collision.gameObject.TryGetComponent(out BaseHealthSystem healthSystem))
+					{
 						healthSystem.TakeDamage();
+						_trailRenderer.enabled = false;
+					}
 
 					break;
 				}
 				case BulletHitEffect.Slowness:
 				{
 					if (collision.gameObject.TryGetComponent(out PlayerMovement movement))
+					{
 						movement.ApplySlowness();
+						Dispose();
+					}
 
 					break;
 				}
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
-
-			Dispose();
 		}
 
 		private void Dispose()
@@ -70,17 +84,16 @@ namespace Guns
 
 	public enum Target
 	{
-		Enemy, Player
+		Enemy,
+		Player
 	}
 
-	// For debugging
-	[Serializable]
-	public struct BulletData
+	public readonly struct BulletData
 	{
-		public float Timeout;
-		public float Speed;
-		public Vector3 Origin;
-		public Target Target;
+		public float Timeout { get; }
+		public float Speed { get; }
+		public Vector3 Origin { get; }
+		public Target Target { get; }
 
 		public BulletData(Target target, Vector3 origin, float speed, float timeout)
 		{
